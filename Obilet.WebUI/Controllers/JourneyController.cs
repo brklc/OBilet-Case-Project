36694d8cc66accc5895711cfcd3a84fec6fdf38c
Burak Kılıç <burak.kilic@sys.com.tr>
2022-02-09 13:36:17 +0300

    changed method async

--- Obilet.WebUI/Controllers/JourneyController.cs
+++ Obilet.WebUI/Controllers/JourneyController.cs
@@ -6,6 +6,7 @@ using Obilet.Core.Models;
 using Obilet.WebUI.Helpers;
 using Obilet.WebUI.ViewModels;
 using System;
+using System.Threading.Tasks;
 
 namespace Obilet.WebUI.Controllers
 {
@@ -14,17 +15,17 @@ namespace Obilet.WebUI.Controllers
         private readonly ISessionService _sessionService;
         private readonly ILocationService _locationService;
         private readonly IJourneyService _journeyService;
-        public JourneyController(ISessionService sessionService, ILocationService locationService,IJourneyService journeyService)
+        public JourneyController(ISessionService sessionService, ILocationService locationService, IJourneyService journeyService)
         {
             _sessionService = sessionService;
             _locationService = locationService;
             _journeyService = journeyService;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            SetOrGetSession();
-            
-            var busLocations = _locationService.GetBusLocations(new BusLocation() { DeviceSession = new DeviceSession() { DeviceId = SessionHelper.Get(HttpContext.Session, "DeviceId"), SessionId = SessionHelper.Get(HttpContext.Session, "SessionId") },Data= null,Date = DateTime.Now,Language = "tr-TR" });
+            await SetOrGetSession();
+
+            var busLocations = _locationService.GetBusLocations(new BusLocation() { DeviceSession = new DeviceSession() { DeviceId = SessionHelper.Get(HttpContext.Session, "DeviceId"), SessionId = SessionHelper.Get(HttpContext.Session, "SessionId") }, Data = null, Date = DateTime.Now, Language = "tr-TR" });
 
             SelectList busLocationList = new SelectList(busLocations.Result.Data, "Id", "Name");
 
@@ -32,14 +33,14 @@ namespace Obilet.WebUI.Controllers
             {
                 DataSelectList = busLocationList
             };
-      
+
             return View(busLocationViewModel);
         }
 
         [HttpPost]
         public IActionResult SearchTicket(int originId, int destinationId, DateTime date)
         {
-            var journeyList = _journeyService.GetBusJourneys(new BusJourney() { Data = new Data() { OriginId = originId, DestinationId = destinationId,DepartureDate = date},Date = DateTime.Now, DeviceSession= new DeviceSession() { DeviceId = SessionHelper.Get(HttpContext.Session, "DeviceId"), SessionId = SessionHelper.Get(HttpContext.Session, "SessionId") },Language="tr-TR"});
+            var journeyList = _journeyService.GetBusJourneys(new BusJourney() { Data = new Data() { OriginId = originId, DestinationId = destinationId, DepartureDate = date }, Date = DateTime.Now, DeviceSession = new DeviceSession() { DeviceId = SessionHelper.Get(HttpContext.Session, "DeviceId"), SessionId = SessionHelper.Get(HttpContext.Session, "SessionId") }, Language = "tr-TR" });
 
             BusJourneyViewModel busJourneyViewModel = new BusJourneyViewModel()
             {
@@ -48,16 +49,18 @@ namespace Obilet.WebUI.Controllers
             return View(busJourneyViewModel);
         }
 
-        public async void SetOrGetSession()
+        public async Task SetOrGetSession()
         {
             if (String.IsNullOrEmpty(SessionHelper.Get(HttpContext.Session, "DeviceId")) && String.IsNullOrEmpty(SessionHelper.Get(HttpContext.Session, "SessionId")))
             {
                 var session = await _sessionService.GetSession();
+               
                 SessionHelper.Set(HttpContext.Session, "DeviceId", session.Data.DeviceId);
                 SessionHelper.Set(HttpContext.Session, "SessionId", session.Data.SessionId);
             }
+
         }
 
-      
+
     }
 }
